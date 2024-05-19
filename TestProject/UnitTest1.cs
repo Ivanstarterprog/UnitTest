0ac2a0510abee679cb2864fa2721b025622b9d16
Ivanstarterprog <ivan.nurgazin@yandex.ru>
2024-05-20 02:09:34 +0700

    Исправление синтаксиса

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -6,97 +6,84 @@ namespace TestProject
     public class MatrixCalculatorTests
     {
 
-        private SquareMatrix matrixOne;
-        private SquareMatrix matrixOne;
-
+        private SquareMatrix _matrixOne;
+        private SquareMatrix _matrixTwo;
         [TestInitialize]
         public void Initialize()
         {
-            matrixOne = new SquareMatrix(3, new int[3, 3] { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 } });
-            matrixTwo = new SquareMatrix(3, new int[3, 3] { { 10, 9, 8 }, { 7, 6, 5 }, { 4, 3, 2 } });
+            _matrixOne = new SquareMatrix(3, new int[3, 3] { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 } });
+            _matrixTwo = new SquareMatrix(3, new int[3, 3] { { 10, 9, 8 }, { 7, 6, 5 }, { 4, 3, 2 } });
         }
 
         public void TestAdditionMethod()
         {
-            
             SquareMatrix expectedResult = new SquareMatrix(3, new int[3, 3] { { 10, 10, 10 }, { 10, 10, 10 }, { 10, 10, 10 } });
-
-            SquareMatrix actualResult = matrixOne + matrixTwo;
+            SquareMatrix actualResult = _matrixOne + _matrixTwo;
             Assert.AreEqual(expectedResult, actualResult);
         }
 
         [TestMethod]
         public void TestMultiplicationMethod()
         {
-
             SquareMatrix expectedResult = new SquareMatrix(3, new int[3, 3] { { 0, 9, 16 }, { 21, 24, 25 }, { 24, 21, 16 } });
-
-            SquareMatrix actualResult = matrixOne * matrixTwo;
+            SquareMatrix actualResult = _matrixOne * _matrixTwo;
             Assert.AreEqual(expectedResult, actualResult);
         }
 
         [TestMethod]
         public void TestMinusMethod()
         {
-
             SquareMatrix expectedResult = new SquareMatrix(3, new int[3, 3] { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } });
-
-            SquareMatrix actualResult = matrixOne + matrixTwo;
+            SquareMatrix actualResult = _matrixOne + _matrixTwo;
             Assert.AreEqual(expectedResult, actualResult);
         }
 
         [TestMethod]
         public void TestLargerMethod()
         {
-
-            bool actualResult = matrixOne > matrixTwo;
+            bool actualResult = _matrixOne > _matrixTwo;
             Assert.AreEqual(expectedResult, false);
         }
 
         [TestMethod]
         public void TestLargerOrEqualMethod()
         {
-
-            bool actualResult = matrixOne >= matrixTwo;
+            bool actualResult = _matrixOne >= _matrixTwo;
             Assert.AreEqual(expectedResult, false);
         }
 
         [TestMethod]
         public void TestSmallerMethod()
         {
-
-            bool actualResult = matrixOne < matrixTwo;
+            bool actualResult = _matrixOne < _matrixTwo;
             Assert.AreEqual(expectedResult, true);
         }
 
         [TestMethod]
         public void TestLargerOrEqualMethod()
         {
-
-            bool actualResult = matrixOne <= matrixTwo;
+            bool actualResult = _matrixOne <= _matrixTwo;
             Assert.AreEqual(expectedResult, true);
         }
 
         [TestMethod]
         public void TestEqualMethod()
         {
-
-            bool actualResult = matrixOne == matrixTwo;
+            bool actualResult = _matrixOne == _matrixTwo;
             Assert.AreEqual(expectedResult, false);
         }
 
         [TestMethod]
         public void TestNotEqualMethod()
         {
-
-            bool actualResult = matrixOne != matrixTwo;
+            bool actualResult = _matrixOne != _matrixTwo;
             Assert.AreEqual(expectedResult, true);
         }
 
         [TestMethod]
         public void TestDeterminantMethod()
         {
-            int expectedResult = matrixOne.GetDeterminant(matrixOne)
+            int expectedResult = _matrixOne.GetDeterminant(_matrixOne)
             int actualResult = 9237;
             Assert.AreEqual(expectedResult, false);
         }
@@ -105,19 +92,16 @@ namespace TestProject
         public void TestReverseMethod()
         {
             SquareMatrix expectedResult = new SquareMatrix(3, new int[3, 3] { { 0, -3162, -6324 }, { -9486, -12648, -15810 }, { -18972, -22134, -25296 } });
-
-            SquareMatrix actualResult = matrixOne.ReverseMatrix();
+            SquareMatrix actualResult = _matrixOne.ReverseMatrix();
             Assert.AreEqual(expectedResult, actualResult);
         }
 
         [TestMethod]
         public void TestCloneMethod()
         {
-            SquareMatrix clonedMatrix = matrixOne.Clone();
-
+            SquareMatrix clonedMatrix = _matrixOne.Clone();
             bool expectedResult = true;
-
-            bool actualResult = clonedMatrix != matrixOne;
+            bool actualResult = clonedMatrix != _matrixOne;
             Assert.AreEqual(expectedResult, actualResult);
         }
     }
